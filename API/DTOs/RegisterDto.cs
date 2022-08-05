306020c3faad2b9956a3de49b9947ba74010d9f7
GaborHajdu83 <hajdu.gabor1983@gmail.com>
2022-08-05 16:26:17 +0200

    End of section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,9 +4,15 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
+        [Required] public string Username { get; set; } = string.Empty;
+        [Required] public string KonwnAs { get; set; } = string.Empty;
+        [Required] public string Gender { get; set; } = string.Empty;
+        [Required] public DateTime DateOfBirth { get; set; }
+        [Required] public string City { get; set; } = string.Empty;
+        [Required] public string Country { get; set; } = string.Empty;
+
         [Required]
-        public string Username { get; set; } = string.Empty;
-        [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; } = string.Empty;
     }
 }
